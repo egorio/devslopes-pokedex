7c2898c223b42ffcb3a565a5d510aabd9e305798
egorio <justegorio@gmail.com>
2016-03-08 15:03:22 -0800

    Feat: Allow user to start and stop background music.
    Add music icon at the top right corner.
    Add application title with Pokemon Solid font.

--- devslopes-pokedex/ViewController.swift
+++ devslopes-pokedex/ViewController.swift
@@ -7,31 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collection: UICollectionView!
 
     var pokemons = [Pokemon]()
-    
+    var musicPlayer: AVAudioPlayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collection.delegate = self
         collection.dataSource = self
-        
+
         parsePokemonCSV()
+        initAudio()
     }
     
-    func parsePokemonCSV() {
-        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+    func initAudio() {
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
         
         do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+    }
+
+    func parsePokemonCSV() {
+        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+
+        do {
             let csv = try CSV(contentsOfURL: path)
-            
+
             for row in csv.rows {
                 let pokemon = Pokemon(pokedexId: Int(row["id"]!)!, name: row["identifier"]!)
-                
+
                 pokemons.append(pokemon)
             }
         } catch let error as NSError {
@@ -64,5 +79,16 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         let size = (collectionView.bounds.width - 2 * 10) / 3
         return CGSize(width: size, height: size)
+    }
+    
+    @IBAction func musicToggle(sender: UIButton!) {
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 1
+        }
+        else {
+            musicPlayer.play()
+            sender.alpha = 0.2
+        }
     }
 }
